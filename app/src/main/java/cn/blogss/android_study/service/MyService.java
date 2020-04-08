e7a7924dbf9f35702d66336d3ca216ee9a32d9fc
liqiang <liqiang8102@163.com>
2020-04-08 17:30:10 +0800

    【2020.4.8】【1.优化通知渠道代码 2.新建helper模块】

--- app/src/main/java/cn/blogss/android_study/service/MyService.java
+++ app/src/main/java/cn/blogss/android_study/service/MyService.java
@@ -1,11 +1,13 @@
 package cn.blogss.android_study.service;
 
+import android.annotation.TargetApi;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Binder;
 import android.os.Build;
 import android.os.IBinder;
@@ -28,32 +30,45 @@ public class MyService extends Service {
 
     private final static String CHANNEL_ID = "cn.blogss.android_study.n1";
 
-    private final static String CHANNEL_NAME = "n1";
+    private final static String CHANNEL_NAME = "聊天消息";
+
+    NotificationManager notificationManager;
 
     @Override
     public void onCreate() {
         super.onCreate();
         Log.d(TAG,"onCreate");
-        NotificationChannel notificationChannel = null;
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){/*Android 8.0及其以上*/
-            notificationChannel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH);
-            NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-            notificationManager.createNotificationChannel(notificationChannel);
+        notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){/*确保的是当前手机的系统版本必须是Android 8.0系统或者更高，
+        因为低版本的手机系统并没有通知渠道这个功能，不做系统版本检查的话会在低版本手机上造成崩溃。*/
+            createNotificationChannel();
         }
-        Intent notificationIntent = new Intent(this,MainActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,notificationIntent,0);
-        Notification notification = getNotification(pendingIntent);
-
-        startForeground(1, notification);//让Service变成前台Service,并在系统的状态栏显示出来
+        Notification notification = getNotification();
+        notificationManager.notify(1,notification);
+        //startForeground(1, notification);//让Service变成前台Service,并在系统的状态栏显示出来
     }
 
-    private Notification getNotification(PendingIntent pendingIntent) {
+    /*创建一个通知渠道至少需要渠道ID、渠道名称以及重要等级这三个参数，其中渠道ID可以随便定义，只要保证全局唯一性就可以。
+        渠道名称是给用户看的，需要能够表达清楚这个渠道的用途。重要等级的不同则会决定通知的不同行为，
+        当然这里只是初始状态下的重要等级，用户可以随时手动更改某个渠道的重要等级，App是无法干预的
+    */
+    @TargetApi(Build.VERSION_CODES.O)
+    private void createNotificationChannel() {
+        NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH);
+        notificationManager.createNotificationChannel(notificationChannel);
+    }
+
+
+    private Notification getNotification() {
+        Intent notificationIntent = new Intent(this,MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,notificationIntent,0);
         //新建Builer对象
         return new NotificationCompat.Builder(this,CHANNEL_ID).
         setContentTitle(getApplicationContext().getResources().getString(R.string.service_content_title)).//设置通知的标题
         setContentText(getApplicationContext().getResources().getString(R.string.service_content)).//设置通知的内容
         setWhen(System.currentTimeMillis()).
         setSmallIcon(R.mipmap.ic_launcher).//设置通知的图标
+        setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher)).
         setContentIntent(pendingIntent).build();
     }
 
