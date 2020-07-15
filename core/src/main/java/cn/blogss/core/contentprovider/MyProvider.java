095954977fced5d097ba7a454054e5f1ab83d819
liqiang <liqiang8102@163.com>
2020-07-15 15:42:11 +0800

    【2020.7.15】【1.自创建内容提供器完整代码】

--- core/src/main/java/cn/blogss/core/contentprovider/MyProvider.java
+++ core/src/main/java/cn/blogss/core/contentprovider/MyProvider.java
@@ -63,19 +63,37 @@ class MyProvider extends ContentProvider {
     }
 
     /**
-     * 根据传入的内容URI返回相应的MIME类型
-     * 一个标准的内容URI写法是这样的：content://cn.blogss.android_study/table1
+     * 根据传入的内容 URI 返回相应的 MIME 类型
+     *
+     * 一个标准的内容 URI 写法是这样的：content://cn.blogss.android_study/table1
      * 这就表示调用方期望访问的是 cn.blogss.android_study 这个应用的 table1 表中的数据
      * content://cn.blogss.android_study/table1/1, 调用方期望访问这个应用 table1 表中 id 为 1 的数据
      * content://cn.blogss.android_study/*, 一个能匹配任意表的内容 URI, (*表示匹配任意长度的任意字符)
      * content://cn.blogss.android_study/table1/#, 一个能匹配 table1 表中任意一行数据的的内容 URI, (#表示匹配任意长度的数字)
+     *
+     * 一个 URI 所对应的 MIME 字符串主要由 3 部分组成，Android 对这 3 部分做了如下的规定：
+     * 1. 必须以 vnd 开头
+     * 2. 如果 URI 以路径结尾，则后接 android.cursor.dir/，如果内容 URI 以 id 结尾，则后接 android.cursor.item/
+     * 3.最后加上 vnd.<authority>.<path>
      * @param uri
      * @return
      */
     @Nullable
     @Override
     public String getType(@NonNull Uri uri) {
-        return null;
+        switch (uriMatcher.match(uri)){
+            case TABLE1_DIR:
+                return "vnd.android.cursor.dir/vnd.cn.blogss.android_study.table1";
+            case TABLE1_ITEM:
+                return "vnd.android.cursor.item/vnd.cn.blogss.android_study.table1";
+            case TABLE2_DIR:
+                return "vnd.android.cursor.dir/vnd.cn.blogss.android_study.table2";
+            case TABLE2_ITEM:
+                return "vnd.android.cursor.item/vnd.cn.blogss.android_study.table2";
+            default:
+                break;
+        }
+        return "";
     }
 
     @Nullable
