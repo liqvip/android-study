c9569a7a872ec5c50e01c159fb078cce60bedc53
liqiang <liqiang8102@163.com>
2020-07-21 23:31:43 +0800

    【2020.7.21】【1.增加Kotlin】

---
+++ core/src/main/java/cn/blogss/core/kotlin/MainActivity.kt
@@ -0,0 +1,10 @@
+package cn.blogss.core.kotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
