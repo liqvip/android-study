53b66917e336f8f6b18b490f30f2bfaba8bb70d0
liqiang <liqiang8102@163.com>
2020-08-24 13:55:43 +0800

    【2020.8.24】【1.增加一个window窗口工具类 2.完善window代码】

--- core/src/main/java/cn/blogss/core/window/MainActivity.kt
+++ core/src/main/java/cn/blogss/core/window/MainActivity.kt
@@ -1,51 +1,74 @@
 package cn.blogss.core.window
 
-import android.content.Context
-import android.graphics.PixelFormat
-import android.view.Gravity
 import android.view.View
 import android.view.WindowManager
 import android.widget.Button
 import cn.blogss.core.R
 import cn.blogss.core.base.BaseActivity
+import cn.blogss.helper.createWindow
+import cn.blogss.helper.delWindow
 
 class MainActivity : BaseActivity(), View.OnClickListener {
-    private lateinit var btCreate : Button
+    private lateinit var btAppCreate : Button
+    private lateinit var btSubCreate : Button
+    private lateinit var btSysCreate : Button
+    private lateinit var btDel : Button
+
+    private lateinit var floatingAppButton: Button
+    private lateinit var floatingSubButton: Button
+    private lateinit var floatingSysButton: Button
+
+    private var windowViewList = mutableListOf<Button>()
 
     override fun getLayoutId(): Int {
         return R.layout.activity_window
     }
 
     override fun initView() {
-        btCreate = findViewById(R.id.bt_create_one_window)
+        btAppCreate = findViewById(R.id.bt_create_application_window)
+        btSubCreate = findViewById(R.id.bt_create_sub_window)
+        btSysCreate = findViewById(R.id.bt_create_sys_window)
+        btDel = findViewById(R.id.bt_del_window)
 
-        btCreate.setOnClickListener(this)
+        btAppCreate.setOnClickListener(this)
+        btSubCreate.setOnClickListener(this)
+        btSysCreate.setOnClickListener(this)
+        btDel.setOnClickListener(this)
+
+        floatingAppButton = Button(this)
+        floatingSubButton = Button(this)
+        floatingSysButton = Button(this)
     }
 
     override fun onClick(v: View?) {
         when (v!!.id) {
-            R.id.bt_create_one_window -> {
-                createOneWindow()
+            R.id.bt_create_application_window -> {
+                if(windowViewList.contains(floatingAppButton))
+                    return
+                windowViewList.add(floatingAppButton)
+                floatingAppButton.text = "应用 Window"
+                createWindow(floatingAppButton,200,300,WindowManager.LayoutParams.FIRST_APPLICATION_WINDOW,this)
+            }
+            R.id.bt_create_sub_window -> {
+                if(windowViewList.contains(floatingSubButton))
+                    return
+                windowViewList.add(floatingSubButton)
+                floatingSubButton.text = "子 Window"
+                createWindow(floatingSubButton,200,300,WindowManager.LayoutParams.FIRST_SUB_WINDOW,this)
+            }
+            R.id.bt_create_sys_window -> {
+                if(windowViewList.contains(floatingSysButton))
+                    return
+                windowViewList.add(floatingSysButton)
+                floatingSysButton.text = "系统 Window"
+                createWindow(floatingSysButton,200,300,WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,this)
+            }
+            R.id.bt_del_window -> {
+                windowViewList.forEach {
+                    delWindow(it,this)
+                }
+                windowViewList.clear()
             }
         }
     }
-
-    /**
-     * 使用 WindowManager 添加一个 Window
-     */
-    private fun createOneWindow() {
-        val floatingButton = Button(this)
-        val windowManager: WindowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
-        floatingButton.text = "Button"
-        val layoutParams: WindowManager.LayoutParams = WindowManager.LayoutParams(WindowManager.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.TYPE_APPLICATION, 0, PixelFormat.TRANSPARENT)
-        layoutParams.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL or
-                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE or
-                WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
-        layoutParams.gravity = Gravity.START or Gravity.TOP
-        layoutParams.x = 100
-        layoutParams.y = 300
-        windowManager.addView(floatingButton, layoutParams)
-    }
-
 }
---
+++ helper/src/main/java/cn/blogss/helper/WindowCRUD.kt
@@ -0,0 +1,43 @@
+package cn.blogss.helper
+
+import android.content.Context
+import android.graphics.PixelFormat
+import android.view.Gravity
+import android.view.View
+import android.view.WindowManager
+
+/**
+ * @创建人 560266
+ * @文件描述 Window 窗口的 CRUD 操作
+ * @创建时间 2020/8/24
+ */
+
+/**
+ * 创建一个 window
+ */
+fun createWindow(view: View, x: Int, y: Int, windowType: Int, context: Context){
+    val windowManager: WindowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+    val layoutParams: WindowManager.LayoutParams = WindowManager.LayoutParams(WindowManager.LayoutParams.WRAP_CONTENT,
+            WindowManager.LayoutParams.WRAP_CONTENT, windowType, 0, PixelFormat.TRANSPARENT)
+    layoutParams.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL or
+            WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE or
+            WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+    layoutParams.gravity = Gravity.START or Gravity.TOP
+    layoutParams.x = x
+    layoutParams.y = y
+    windowManager.addView(view, layoutParams)
+}
+
+
+/**
+ * 删除 window
+ */
+fun delWindow(view: View, context: Context){
+    val windowManager: WindowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+    windowManager.removeView(view)
+}
+
+fun updateWindow(view: View, context: Context, layoutParams: WindowManager.LayoutParams){
+    val windowManager: WindowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+    windowManager.updateViewLayout(view,layoutParams)
+}
