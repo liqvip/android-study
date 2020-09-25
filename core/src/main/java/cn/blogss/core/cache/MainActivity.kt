995fc837c6a91aeef6b55471d588ac3feb2c71b9
liqiang <liqiang8102@163.com>
2020-09-25 18:30:18 +0800

    【2020.9.25】【1.新增缓存文档 2.中秋活动快乐】

---
+++ core/src/main/java/cn/blogss/core/cache/MainActivity.kt
@@ -0,0 +1,16 @@
+package cn.blogss.core.cache
+
+import cn.blogss.core.R
+import cn.blogss.core.base.BaseActivity
+
+class MainActivity : BaseActivity() {
+
+    override fun getLayoutId(): Int {
+        return R.layout.activity_cache
+    }
+
+    override fun initView() {
+        TODO("Not yet implemented")
+    }
+
+}
