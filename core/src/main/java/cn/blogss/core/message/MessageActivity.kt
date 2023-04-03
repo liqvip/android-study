e48dfa080c0e31042e43ccc9598f2a5a7b729a42
Thatcher <liqiang8102@163.com>
2023-04-03 11:21:52 +0800

    HandlerThread 使用

--- core/src/main/java/cn/blogss/core/message/MessageActivity.kt
+++ core/src/main/java/cn/blogss/core/message/MessageActivity.kt
@@ -1,13 +1,15 @@
 package cn.blogss.core.message
 
-import android.os.Bundle
 import android.os.Handler
+import android.os.HandlerThread
 import android.os.Message
 import android.util.Log
-import cn.blogss.core.R
-import cn.blogss.helper.base.BaseActivity
+import android.view.LayoutInflater
+import cn.blogss.core.databinding.ActivityMessageBinding
+import cn.blogss.helper.base.jetpack.BaseActivity
+import cn.blogss.helper.base.jetpack.BaseViewModel
 
-class MessageActivity : BaseActivity() {
+class MessageActivity : BaseActivity<ActivityMessageBinding, BaseViewModel>() {
     private val runTask = Runnable {
         Log.i(TAG, Thread.currentThread().name+" execute run method.")
     }
@@ -22,8 +24,16 @@ class MessageActivity : BaseActivity() {
         private const val TAG = "MessageActivity"
     }
 
-    override fun getLayoutId(): Int {
-        return R.layout.activity_message
+    private val handlerThread = HandlerThread("message-handler-thread")
+    private lateinit var myHandler: Handler
+    private val myMainHandler = Handler()
+
+    override fun getViewModel(): BaseViewModel? {
+        return null
+    }
+
+    override fun getViewBinding(inflater: LayoutInflater): ActivityMessageBinding {
+        return ActivityMessageBinding.inflate(inflater)
     }
 
     override fun initView() {
@@ -41,8 +51,26 @@ class MessageActivity : BaseActivity() {
         }.start()
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        setTag(TAG)
-        super.onCreate(savedInstanceState)
+    override fun bindObserver() {
+    }
+
+    override fun initData() {
+        myHandler = Handler(handlerThread.looper) {
+            // 子线程模拟耗时操作
+            Thread.sleep(2000)
+            myMainHandler.post {
+                // 主线程更新 UI
+            }
+            true
+        }
+
+        // 在需要的地方发送消息，会发送到 myHandler 的 handleMessage 方法 中
+        myHandler.sendMessage(Message.obtain())
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        // 释放 HandlerThread
+        handlerThread.quitSafely()
     }
 }
