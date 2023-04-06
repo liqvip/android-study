79109c9f1948678968d4579fec12dca0dea4d161
Thatcher <liqiang8102@163.com>
2023-04-06 11:26:08 +0800

    UI 卡顿检测

--- helper/src/main/java/cn/blogss/helper/base/BaseApplication.kt
+++ helper/src/main/java/cn/blogss/helper/base/BaseApplication.kt
@@ -3,9 +3,12 @@ package cn.blogss.helper.base
 import android.app.Application
 import android.content.Context
 import android.util.Log
+import android.view.Choreographer
 import cn.blogss.core.base.CrashHandler
 
 open class BaseApplication : Application() {
+    private val frameCallback = FrameCallback()
+
     companion object {
         private const val TAG = "BaseApplication"
         private lateinit var context: Context
@@ -20,5 +23,27 @@ open class BaseApplication : Application() {
         Log.i(TAG, "onCreate")
         CrashHandler.getInstance().init(this)
         context = applicationContext
+
+        // UI 卡顿检测
+        Choreographer.getInstance().postFrameCallback(frameCallback)
+    }
+
+    class FrameCallback: Choreographer.FrameCallback {
+        private var lastTime = 0L
+        override fun doFrame(frameTimeNanos: Long) {
+            if(lastTime == 0L) {
+                return
+            } else {
+                // 计算UI 绘制间隔
+                val interval = (frameTimeNanos - lastTime)/1000000
+                // 计算丢帧数
+                val loseFrames = interval / 16
+                if(interval > 16) { // UI 绘制间隔超过 16ms, 认为 UI 线程操作超时
+                    Log.i(TAG, "UI 线程超时: ${interval}ms, 丢帧 $loseFrames")
+                }
+            }
+            lastTime = frameTimeNanos
+            Choreographer.getInstance().postFrameCallback(this)
+        }
     }
 }
