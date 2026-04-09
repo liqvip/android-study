e2d3141e88cc05e284bd540b7969f1e113a8e3de
liqiang <liqiang@makeblock.com>
2026-04-09 19:00:01 +0800

    feat：http 网络请求

--- open_lib/src/main/java/cn/blogss/frame/okhttp/OkHttpAct.kt
+++ open_lib/src/main/java/cn/blogss/frame/okhttp/OkHttpAct.kt
@@ -4,9 +4,16 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import cn.blogss.frame.components.LogScreen
+import cn.blogss.helper.LogRepository
+import okhttp3.Request
+
+private const val baseUrl = "https://api.52vmy.cn/api/img/tu/girl"
 
 class OkHttpAct: AppCompatActivity() {
 
@@ -25,6 +32,19 @@ class OkHttpAct: AppCompatActivity() {
 @Composable
 fun Body(){
     Column {
+        Row {
+            Button(onClick = {
+                val request = Request.Builder()
+                    .url(baseUrl)
+                    .build()
+                OkHttpManager.instance.get(request, {
+                    val body = it.body()?.string() ?: "响应为空"
+                    LogRepository.addLog(body)
+                })
+            }) {
+                Text("获取女神照片")
+            }
+        }
         LogScreen()
     }
 }
--- open_lib/src/main/java/cn/blogss/frame/okhttp/OkHttpManager.kt
+++ open_lib/src/main/java/cn/blogss/frame/okhttp/OkHttpManager.kt
@@ -1,14 +1,37 @@
 package cn.blogss.frame.okhttp
 
+import okhttp3.Call
+import okhttp3.Callback
 import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import java.io.IOException
 import java.util.concurrent.TimeUnit
 
-class OkHttpManager {
+class OkHttpManager private constructor(){
+    /**
+     * 默认的连接超时、读取超时、写入超时都为 10s
+     */
     private val client: OkHttpClient = OkHttpClient.Builder()
         .connectTimeout(60, TimeUnit.SECONDS)
         .build()
 
     companion object {
         val instance by lazy { OkHttpManager() }
+    }
+
+
+    fun get(request: Request, onResponse: (response: Response) -> Unit,
+            onFailure: (e: IOException) -> Unit = { it.printStackTrace() }
+        ){
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                onFailure(e)
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                onResponse(response)
+            }
+        })
     }
 }
