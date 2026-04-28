b13624c4223dbaf1351d80aac884307ce6e1ab57
liqiang <liqiang@makeblock.com>
2026-04-28 17:54:45 +0800

    feat：带接收者的函数底层实现原理

--- kotlin/src/main/java/cn/blogss/kotlin/带有接收者的函数类型.kt
+++ kotlin/src/main/java/cn/blogss/kotlin/带有接收者的函数类型.kt
@@ -1,6 +1,27 @@
 package cn.blogss.kotlin
 
+data class Person(val name: String, var age: Int)
 
+
+/**
+ * 下面是 add 函数反编译成 java 后的的代码
+ * add 实际变成了 add$lambda$0(Person var0) 的形式
+ * 将接收者对象作为参数传入了 add$lambda$0 方法
+ *
+ *    private static final Unit add$lambda$0(Person var0) {
+ *       Intrinsics.checkNotNullParameter(var0, "<this>");
+ *       var0.setAge(var0.getAge() + 1);
+ *       return Unit.INSTANCE;
+ *    }
+ */
+val add: Person.() -> Unit = {
+    age += 1
+}
 
 fun main() {
+    val person = Person("John", 25)
+    // 输出: Name: John, Age: 25
+    println("Name: ${person.name}, Age: ${person.age}")
+    // 在 Lambda 表达式中，可以直接访问 Person 类的成员
+    person.add()
 }
--- kotlin/src/main/java/cn/blogss/kotlin/扩展函数之apply.kt
+++ kotlin/src/main/java/cn/blogss/kotlin/扩展函数之apply.kt
@@ -6,6 +6,8 @@ package cn.blogss.kotlin
  * apply 扩展函数只有一个参数，参数类型是带有接收者的函数类型
  * 这样在 lambda 表达式中就可以访问接收者对象的成员变量
  *
+ * 使用场景：在需要配置对象属性的时候
+ *
  */
 data class Dog(var name: String = "小白", val age: Int = 2) {
     val des: String
@@ -14,6 +16,7 @@ data class Dog(var name: String = "小白", val age: Int = 2) {
 
 fun main() {
     val dog = Dog(age = 5).apply {
+        // lambda 中可以直接访问接收者对象的成员
         name = "小黑"
     }
     println(dog.des)
