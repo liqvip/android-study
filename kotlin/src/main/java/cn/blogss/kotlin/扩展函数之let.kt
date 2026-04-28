1871378ce142fe373acc5abd2957ecc56551d558
liqiang <liqiang@makeblock.com>
2026-04-28 18:29:23 +0800

    feat：[kotlin] 扩展函数之 let

--- kotlin/src/main/java/cn/blogss/kotlin/扩展函数之let.kt
+++ kotlin/src/main/java/cn/blogss/kotlin/扩展函数之let.kt
@@ -1,4 +1,15 @@
 package cn.blogss.kotlin
 
-class 扩展函数之let {
+/**
+ * let 函数的返回值是 lambda 表达式的返回值
+ * 可以在 lambda 中使用 it 来访问自身
+ *
+ * 使用场景:处理非空对象，对象映射转换
+ */
+
+fun main() {
+    val dog: Dog? = Dog()
+    dog?.let {
+        println(it.des)
+    }
 }
