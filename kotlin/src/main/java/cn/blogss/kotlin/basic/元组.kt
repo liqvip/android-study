ecf573768c0544b4f27a454f8be3f045539c7172
liq2019 <liqiang8102@163.com>
2026-05-11 01:23:22 +0800

    feat:元组

---
+++ kotlin/src/main/java/cn/blogss/kotlin/basic/元组.kt
@@ -0,0 +1,10 @@
+package cn.blogss.kotlin.basic
+
+/**
+ * [Pair](https://kotlinlang.org/api/core/kotlin-stdlib/kotlin/-pair/)
+ */
+fun main() {
+    val (a, b) = 1 to "x"
+    println(a)
+    println(b)
+}
