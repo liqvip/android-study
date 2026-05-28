4e8f62c2851c308e2ef86418366b2e45547c09a0
liqiang <liqiang@onero.com>
2026-05-28 19:16:51 +0800

    feat：for 循环遍历

--- kotlin/src/main/java/cn/blogss/kotlin/basic/for循环.kt
+++ kotlin/src/main/java/cn/blogss/kotlin/basic/for循环.kt
@@ -5,8 +5,39 @@ package cn.blogss.kotlin.basic
  * 使用 for 循环来遍历一个集合、数组或区间
  */
 fun main() {
-    println("遍历闭区间")
+    println("1.遍历闭区间")
     for (i in 1.rangeTo(6)) {
         println(i)
+    }
+
+    println("2.遍历开区间")
+    for (i in 1.rangeUntil(6)) {
+        println(i)
+    }
+
+    println("3.遍历数组")
+    val arr = arrayOf(1, 2, 3)
+    for (i in arr.indices) {
+        println(arr[i])
+    }
+
+    /**
+     * 遍历集合类型，使用 for 也可以
+     * 但是如果需要删除元素，应该使用迭代器
+     */
+
+    println("4. 遍历列表，使用 for")
+    val list = mutableListOf("1", "2", "3")
+    val iterator = list.iterator()
+
+    for(i in list) {
+        println(i)
+    }
+
+    println("5. 遍历列表，使用迭代器")
+    while (iterator.hasNext()) {
+        val i = iterator.next()
+        if(i == "1")  iterator.remove()
+        println(iterator.next())
     }
 }
